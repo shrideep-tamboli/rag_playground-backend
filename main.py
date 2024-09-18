d74b1957b6c09efc7f7b5bc9920324990a091e73
Shrideep_Tamboli <27.tshrideep@gmail.com>
2024-09-18 12:53:28 +0530

    env variables + vercel.json + build configuration

--- main.py
+++ main.py
@@ -120,15 +120,15 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 ## Environment varibale initialization
-NEO4J_URI = os.getenv("NEO4J_URI")
-NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
-NEO4J_DATABASE = os.getenv("NEO4J_DATABASE")
+#NEO4J_URI = os.getenv("NEO4J_URI")
+#NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+#NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+#NEO4J_DATABASE = os.getenv("NEO4J_DATABASE")
 groq_api_key = os.getenv("groq_api_key")
 OPENAI_API_KEY2 = os.getenv('OPENAI_API_KEY2')
-LANGCHAIN_TRACING_V2 = "true"
-LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
-LANGCHAIN_PROJECT="RAG_Playground"
+#LANGCHAIN_TRACING_V2 = "true"
+#LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+#LANGCHAIN_PROJECT="RAG_Playground"
 
 #### INITIALIZE FT METHODS SENT BY USER
 ## no change for deployment
