ecebe95e5c6a0775db336c6fa991a58f5cfc16f0
Shrideep_Tamboli <27.tshrideep@gmail.com>
2024-09-19 11:17:18 +0530

    /playground

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rag-playground-frontend.vercel.app"],
+    allow_origins=["https://rag-playground-frontend.vercel.app/playground"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
